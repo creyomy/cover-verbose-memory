37131992f38e12e15c6568e746d8ea3dce1a1321
creyomy <54267602+creyomy@users.noreply.github.com>
2021-04-05 12:13:06 -0700

    x
    inc directory done

---
+++ inc/footer.php
@@ -0,0 +1,5 @@
+<footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://www.facebook.com/DefinitionofTrash/">@KYLEDELAVICTORIA</a>.</p>
+        </div>
+      </footer>
